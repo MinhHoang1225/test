5d167c65e354cb507cf13963903d1e61b3ab7615
MinhHoang1225 <minhhoang121225@gmail.com>
2024-09-12 11:56:11 +0700

    done

---
+++ hcn.js
@@ -0,0 +1,11 @@
+// var ch = prompt("nhap chieu dai: ")
+// var cr = prompt("nhap chieu rong: ")
+// var dt = ch * cr;
+// alert(dt)
+// document.write(dt)
+// document.getElementById('kq').inertHTML=dt;
+
+var a = confirm("Do you want to play: ")
+if (a == true){
+    window.ope
+}
